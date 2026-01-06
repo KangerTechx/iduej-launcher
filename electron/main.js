ece229b556fa5c5d8313dcce4150c2c9c2d71407
SEB\sbbla <sbblache@gmail.com>
2026-01-06 15:54:25 +0100

    initial commit

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+const isDev = process.env.NODE_ENV === "development";
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        minWidth: 1280, // Largeur minimale de la fenêtre
+        minHeight: 720, // Hauteur minimale de la fenêtre
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, 'resources/icon.ico'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+        }
+    });
+
+    if(isDev) {
+        win.loadURL("http://localhost:3000")
+    } else {
+        win.loadFile(path.join(__dirname, "../out/index.html"));
+    }
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if(process.platform !=="darwin") app.quit()
+})
